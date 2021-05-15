e89538f136a1aff7818c0ea512eb1cbca3be32ba
NMR <77173929+NMR-Code@users.noreply.github.com>
2021-05-15 12:29:16 -0400

    fixed file structure and made initial html for team

--- Assets/index.js
+++ Assets/index.js
@@ -1,16 +1,9 @@
-const fs = require('fs');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
 const chalk = require('chalk');
-const inquirer = require('inquirer');
-const generateMd = require("./utils/generateMd")
-
-// Welcome message
-const welcome = [{
-    type: 'confirm',
-    prefix: '\b',
-    name: 'welcome',
-    message: chalk.cyanBright(`Thank you for using my Team Builder application! You will be presented with options for your Teams members and their respective details. To begin hit 'Y' or enter.`),
-
-}, ];
 
 // Markdown tips
 const startText = chalk.cyanBright(`\n
@@ -22,154 +15,119 @@ Bold:   **text**
 Links:  [title](https://www.example.com)
 Image:  ![alt text](example.jpg)
 \n`);
+// Welcome message
+const welcome = [{
+    type: 'confirm',
+    prefix: '\b',
+    name: 'welcome',
+    message: chalk.cyanBright(`Thank you for using my Team Builder application! You will be presented with options for your Team members and their respective details. To begin hit 'Y' or enter.`),
 
+}, ];
 // Success message
 const generated = chalk.cyanBright(`
-Your README Generated! It's in the "generatedReadMe" folder
+Your team has been built, in union there is strength!
 //-------------------------------------------------------// 
 `);
+const employees = [];
 
-//Questions for README
-const questions = [
-    // Project name
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of the project? (Req)',
-        validate: titleInput => {
-            if (titleInput) {
-                return true;
-            } else {
-                console.log('You need to enter a title to continue!');
-                return false;
-            }
-        }
-    },
-    // Description
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Provide a description of the project (Req)',
-        validate: descriptionInput => {
-            if (descriptionInput) {
-                return true;
-            } else {
-                console.log('You need to provide a project description!');
-                return false;
-            }
-        }
-    },
-    // Installation
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'How do you install your project? (Req)',
-        validate: installationInput => {
-            if (installationInput) {
-                return true;
-            } else {
-                console.log('You need to provide installation info!');
-                return false;
-            }
-        }
-    },
-    // Usage Info
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'How do you run this project? (Req)',
-        validate: usageInput => {
-            if (usageInput) {
-                return true;
-            } else {
-                console.log('You must provide information on how to use the program!');
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'contribution',
-        message: 'How can people contribute to this project? (Req)',
-        validate: contributionInput => {
-            if (contributionInput) {
-                return true;
-            } else {
-                console.log('You need to provide information on how to contribute to the project!');
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'testing',
-        message: 'How do you test this project? (Req)',
-        validate: testingInput => {
-            if (testingInput) {
-                return true;
-            } else {
-                console.log('You need to describe how to test this project!');
-                return false;
-            }
-        }
-    },
-    // Licensing
-    {
-        type: 'checkbox',
-        name: 'licensing',
-        message: 'Choose a license for your project (Req)',
-        choices: ['Apache', 'MIT', 'Mozilla-Public', 'GNU-General-Public', 'Common-Development-and Distribution', 'None'],
-        validate: licensingInput => {
-            if (licensingInput) {
-                return true;
-            } else {
-                console.log('You must pick a license for the project!');
-                return false;
-            }
-        }
-    },
-    // Github
-    {
-        type: 'input',
-        name: 'github',
-        message: 'GitHub Username (Req)',
-        validate: githubInput => {
-            if (githubInput) {
-                return true;
-            } else {
-                console.log('Please enter your GitHub username!');
-                return false;
-            }
-        }
-    },
-    // Email
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Email address?',
-    },
-];
-
-//Function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, err => {
-        if (err) {
-            return console.log(err);
-        }
-        console.log('Your markdown file has been created and stored in the folder.')
-    });
+function initApp() {
+    startHtml();
+    addMember();
 }
+//Add teamMember f()
+function addMember() {
+    inquirer.prompt([{
+                message: "Team member's name?",
+                name: "name"
+            },
+            {
+                type: "list",
+                message: "What is their role?",
+                choices: [
+                    "Manager",
+                    "Engineer",
+                    "Intern"
+                ],
+                name: "role"
+            },
+            {
+                message: "Enter the team member's id",
+                name: "id"
+            },
+            {
+                message: "Enter the team member's email address",
+                name: "email"
+            }
+        ])
+        .then(function({ name, role, id, email }) {
+            let roleInfo = "";
+            if (role === "Engineer") {
+                roleInfo = "GitHub Username";
+            } else if (role === "Intern") {
+                roleInfo = "school name";
+            } else {
+                roleInfo = "Office Phone #";
+            }
+            inquirer.prompt([{
+                        message: `What is this member's ${roleInfo}`,
+                        name: "roleInfo"
+                    },
+                    {
+                        type: "list",
+                        message: "Would you like to add more members?",
+                        choices: [
+                            "yes",
+                            "no"
+                        ],
+                        name: "moreMembers"
+                    }
+                ])
+                .then(function({ roleInfo, moreMembers }) {
+                    let newMember;
+                    if (role === "Engineer") {
+                        newMember = new Engineer(name, id, email, roleInfo);
+                    } else if (role === "Intern") {
+                        newMember = new Intern(name, id, email, roleInfo);
+                    } else {
+                        newMember = new Manager(name, id, email, roleInfo);
+                    }
+                    employees.push(newMember);
+                    addHtml(newMember)
+                        .then(function() {
+                            if (moreMembers === "yes") {
+                                addMember();
+                            } else {
+                                finishHtml();
+                            }
+                        });
 
-//Async Function to initialize the generator
-const init = async() => {
-    try {
-        await inquirer.prompt(welcome);
-        console.log(startText);
-        const data = await inquirer.prompt(questions);
-        writeToFile('README.md', generateMd(data));
-    } catch (err) {
-        console.log(err);
-    }
+                });
+        });
+}
+//Function to write HTML
+function startHtml() {
+    const html = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <title>Team Profile</title>
+    </head>
+    <body>
+        <nav class="navbar navbar-dark bg-dark mb-5">
+            <span class="navbar-brand mb-0 h1 w-100 text-center">Team Profile</span>
+        </nav>
+        <div class="container">
+            <div class="row">`;
+    //initialize HTML
+    fs.writeFile("./output/team.html", html, function(err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+    console.log("start");
 }
 
 //Function call to initialize program
