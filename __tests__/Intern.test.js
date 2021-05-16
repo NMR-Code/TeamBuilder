6ac5f9b3385e5879157fae12f91c02c2361c2bf7
NMR <77173929+NMR-Code@users.noreply.github.com>
2021-05-16 17:58:36 -0400

    Added template code

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,17 @@
+const Intern = require("../lib/Intern");
+
+test("getRole() should return \"Intern\"", () => {
+    const testVal = "Intern";
+    const e = new Intern("Melo", 1, "test@test.com", "UM");
+    expect(e.getRole()).toBe(testVal);
+});
+test("Can set the school via constructor", () => {
+    const testVal = "UM";
+    const e = new Intern("Melo", 1, "test@test.com", testVal);
+    expect(e.school).toBe(testVal);
+});
+test("Can get the school via getSchool()", () => {
+    const testVal = "UM";
+    const e = new Intern("Melo", 1, "test@test.com", testVal);
+    expect(e.getSchool()).toBe(testVal);
+});
